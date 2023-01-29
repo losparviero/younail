9fb7b1fc20dd9cebec8bcb84456c77bc4840fe01
Zubin <me@zubin.org.in>
2023-01-29 18:31:06 +0530

    Init

---
+++ api/bot.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const { Bot, webhookCallback, InputFile } = require("grammy");
+const youtubeRegex = new RegExp(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+|^(www\.)?youtu\.be\/.+/);
+const { URL } = require ("url");
+
+// Bot
+
+const bot = new Bot(process.env.BOT_TOKEN);
+
+// Commands
+
+bot.command("start", async (ctx) => {
+  await ctx.reply("*Welcome!* ✨ Send a YouTube link to get the thumbnail.", { parse_mode: "Markdown" })
+    .then(() => console.log("New user added:", ctx.from))
+    .catch((error) => console.error(error));
+  });
+bot.command("help", async (ctx) => {
+  await ctx.reply("*@anzubo Project.*\n\nThis bot uses the predictable URLs for thumbnails YouTube provides. It may break and stop working if YouTube decides to change this pattern.", { parse_mode: "Markdown" })
+    .catch((error) => console.error(error));
+  });
+
+// Messages
+
+bot
+  .on("msg", async (ctx) => {
+    
+    // Logging
+
+    if (ctx.from.last_name === undefined) {
+      console.log('From:', ctx.from.first_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
+    else { console.log('From:', ctx.from.first_name, ctx.from.last_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
+    console.log("Message:", ctx.msg.text);
+
+    // Logic
+
+    if (!youtubeRegex.test(ctx.msg.text)) {
+      await ctx.reply("Send a valid YouTube link!", { reply_to_message_id: ctx.msg.message_id } ).catch((error) => console.error(error)); }
+    else {
+      let message = ctx.message.text;
+      let match = message.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+|^(www\.)?youtu\.be\/.+/);
+      let link = match[0];
+      if (link.includes(".be/")) {
+        let vid = link.split(".be/")[1];
+        let download_link = ("https://img.youtube.com/vi/" + vid + "/maxresdefault.jpg");
+        await ctx.replyWithPhoto(new InputFile(new URL(download_link)) ).catch((error) => console.error(error)); }
+      else {
+        let vid = link.split("/watch?v=")[1];
+        let download_link = ("https://img.youtube.com/vi/" + vid + "/maxresdefault.jpg");
+        await ctx.replyWithPhoto(new InputFile(new URL(download_link)) ).catch((error) => console.error(error)); }
+      }
+    
+  });
+
+// Function
+
+export default webhookCallback(bot, 'http');
