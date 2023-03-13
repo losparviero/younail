1ca4fdab81aa7d50b8e1e06a753c069dd245c645
Zubin <me@zubin.org.in>
2023-03-14 01:10:51 +0530

    Refactor

--- bot.js
+++ bot.js
@@ -1,59 +1,132 @@
-require('dotenv').config();
-const { Bot, GrammyError, HttpError, InputFile } = require("grammy");
-const youtubeRegex = new RegExp(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+|^(www\.)?youtu\.be\/.+/);
-const { URL } = require ("url");
+require("dotenv").config();
+const { Bot, GrammyError, HttpError } = require("grammy");
+const grabLink = require("youtube-thumbnail-grabber");
+const youtubeRegex = new RegExp(
+  /^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+|^(www\.)?youtu\.be\/.+/
+);
 
 // Bot
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
+// Response
+
+async function responseTime(ctx, next) {
+  const before = Date.now();
+  await next();
+  const after = Date.now();
+  console.log(`Response time: ${after - before} ms`);
+}
+
+bot.use(responseTime);
+
 // Commands
 
 bot.command("start", async (ctx) => {
-    await ctx.reply("*Welcome!* ✨ Send a YouTube link to get the thumbnail.", { parse_mode: "Markdown" } );
-    console.log("New user added:", ctx.from);
-  });
-bot.command("help", async (ctx) => await ctx.reply("*@anzubo Project.*\n\nThis bot uses the predictable URLs for thumbnails YouTube provides. It may break and stop working if YouTube decides to change this pattern.", { parse_mode: "Markdown" } ));
+  if (!ctx.chat.type == "private") {
+    await bot.api.sendMessage(
+      ctx.chat.id,
+      "*Channels and groups are not supported presently.*",
+      { parse_mode: "Markdown" }
+    );
+    return;
+  }
+
+  await ctx
+    .reply("*Welcome!* ✨\n_Send a YouTube link to get the thumbnail._", {
+      parse_mode: "Markdown",
+    })
+    .then(console.log("New user added:\n", ctx.from));
+});
+
+bot.command("help", async (ctx) => {
+  await ctx
+    .reply(
+      "*@anzubo Project.*\n\n_This bot gets thumbnails for YouTube videos.\nSend a link to try it out!_",
+      { parse_mode: "Markdown" }
+    )
+    .then(console.log("Help command sent to", ctx.chat.id));
+});
 
 // Messages
 
-bot
-  .on("msg", async (ctx) => {
-    
-    // Logging
+bot.on("message", async (ctx) => {
+  // Logging
 
-    if (ctx.from.last_name === undefined) {
-      console.log('From:', ctx.from.first_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
-    else { console.log('From:', ctx.from.first_name, ctx.from.last_name, '(@' + ctx.from.username + ')', 'ID:', ctx.from.id); }
-    console.log("Message:", ctx.msg.text);
+  const from = ctx.from;
+  const name =
+    from.last_name === undefined
+      ? from.first_name
+      : `${from.first_name} ${from.last_name}`;
+  console.log(
+    `From: ${name} (@${from.username}) ID: ${from.id}\nMessage: ${ctx.message.text}`
+  );
 
-    // Logic
+  // Logic
 
-    if (youtubeRegex.test(ctx.msg.text)) {
-      let message = ctx.message.text;
-      let match = message.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+|^(www\.)?youtu\.be\/.+/);
-      let link = match[0];
-      if (link.includes(".be/")) { let vid = link.split(".be/")[1];
-        let download_link = ("https://img.youtube.com/vi/" + vid + "/maxresdefault.jpg");
-        await ctx.replyWithPhoto(new InputFile(new URL(download_link)) ); }
-      else { let vid = link.split("/watch?v=")[1];
-        let download_link = ("https://img.youtube.com/vi/" + vid + "/maxresdefault.jpg");
-        await ctx.replyWithPhoto(new InputFile(new URL(download_link)) ); }
+  if (!youtubeRegex.test(ctx.message.text)) {
+    await ctx.reply("*Send a valid YouTube link.*", {
+      parse_mode: "Markdown",
+      reply_to_message_id: ctx.message.message_id,
+    });
+    return;
+  }
+
+  try {
+    async function getLink(ytUrl) {
+      const thumbnailLink = grabLink(ytUrl, "max");
+      await ctx.replyWithPhoto(thumbnailLink, {
+        reply_to_message_id: ctx.message.message_id,
+      });
+    }
+    await getLink(ctx.message.text);
+  } catch (error) {
+    if (error instanceof GrammyError) {
+      if (error.message.includes("Forbidden: bot was blocked by the user")) {
+        console.log("Bot was blocked by the user");
+      } else if (error.message.includes("Call to 'sendPhoto' failed!")) {
+        console.log("Error sending message: ", error);
+        await ctx.reply(`*Error contacting Telegram.*`, {
+          parse_mode: "Markdown",
+          reply_to_message_id: ctx.message.message_id,
+        });
+      } else {
+        await ctx.reply(`*An error occurred: ${error.message}*`, {
+          parse_mode: "Markdown",
+          reply_to_message_id: ctx.message.message_id,
+        });
       }
-    else await ctx.reply("Send a valid YouTube link!", { reply_to_message_id: ctx.msg.message_id } );
-    
-  });
+      console.log(`Error sending message: ${error.message}`);
+      return;
+    } else {
+      console.log(`An error occured:`, error);
+      await ctx.reply(`*An error occurred.*\n_Error: ${error.message}_`, {
+        parse_mode: "Markdown",
+        reply_to_message_id: ctx.message.message_id,
+      });
+      return;
+    }
+  }
+});
 
-// Error Handling
+// Error
 
 bot.catch((err) => {
   const ctx = err.ctx;
-  console.error("Error while handling update", ctx.update.update_id, "\nQuery:", ctx.msg.text, "not found");
-  if (ctx.config.isDeveloper) { ctx.reply("Query: " + ctx.msg.text + " " + "not found!"); }
-  else { bot.api.sendMessage(ctx.config.botDeveloper, "Query: " + ctx.msg.text + " by @" + ctx.from.username + " ID: " + ctx.from.id + " not found!"); }
+  console.error(
+    "Error while handling update",
+    ctx.update.update_id,
+    "\nQuery:",
+    ctx.msg.text
+  );
   const e = err.error;
   if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
+    console.error("Error in request:", e);
+    if (e.description === "Forbidden: bot was blocked by the user") {
+      console.log("Bot was blocked by the user");
+    } else {
+      ctx.reply("An error occurred");
+    }
   } else if (e instanceof HttpError) {
     console.error("Could not contact Telegram:", e);
   } else {
@@ -63,5 +136,4 @@ bot.catch((err) => {
 
 // Run
 
-console.log('Bot running. Please keep this window open or use a startup manager like PM2 to setup persistent execution and store logs.');
 bot.start();
